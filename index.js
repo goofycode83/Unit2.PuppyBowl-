3aa1596292c882169463da509f98fc9286db809d
Kaleab Fekade <kaleab83@gmail.com>
2023-11-09 15:51:56 -0500

    api working and rending on page.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const html = document.querySelector(`html`);
+const body = document.querySelector(`body`);
+const main = document.querySelector(`main`);
+const h1 = document.querySelector(`h1`);
+const ol = document.querySelector(`ol`);
+
+
 const state = {
   playerArray: []
 }
@@ -7,23 +14,15 @@ const getAllPlayers = async () => {
     const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-et-web-ft-sf/players`);
     const result = await response.json();
     const playersInfo = result.data.players;
-    const players = state.playerArray.push(playersInfo);
-    return players;
+    state.playerArray = playersInfo;
+    console.log(state.playerArray);
+    state.playerArray.forEach(user => {
+      const list = `<li>${user.name}</li>`;
+      ol.insertAdjacentHTML(`beforeend`, list);
+    })
+    // renderAllPlayers();
   }catch (err) {
     console.error(err);
   }
 }
 getAllPlayers();
-
-
-const renderAllPlayers = () => {
-  const main = document.querySelector(`main`);
-  const ol = document.createElement(`ol`);
-  const li = document.createElement(`li`);
-  main.appendChild(ol);
-  ol.appendChild(li);
-  console.log(li);
-  li.innerText = getAllPlayers();
-}
-
-renderAllPlayers();
