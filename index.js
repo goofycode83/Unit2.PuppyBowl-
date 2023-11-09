f8ed8c281630bf9a41571d913ee0bf384410833a
Kaleab Fekade <kaleab83@gmail.com>
2023-11-09 16:54:17 -0500

    form added

--- index.js
+++ index.js
@@ -17,12 +17,43 @@ const getAllPlayers = async () => {
     state.playerArray = playersInfo;
     console.log(state.playerArray);
     state.playerArray.forEach(user => {
-      const list = `<li>${user.name}</li>`;
+      const list = `
+      <li>
+      <img src=${user.imageUrl} alt="puppy-picture"><br>
+      <h3> ${user.name} </h3> 
+      </li>`;
       ol.insertAdjacentHTML(`beforeend`, list);
     })
-    // renderAllPlayers();
   }catch (err) {
     console.error(err);
   }
 }
 getAllPlayers();
+
+const form = document.querySelector(`form`);
+console.log(form);
+form.addEventListener(`Submit` , async (event) => {
+  event.preventDefault();
+  const userInput = document.querySelector(`input`); 
+  const inputNumber = input.value;
+  try {
+  const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-et-web-ft-sf/players`, {
+    method: `POST`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: 'Rufus',
+      breed: 'Irish Setter',
+    }),
+  });
+  console.log(response);
+  const newPuppy = await response.json();
+}catch (err) {
+  console.error(err);
+}
+  const newPuppyLI = document.createElement(`li`);
+  newPuppyLI.innerText = newPuppy.name;
+  const ul = document.querySelector(`ul`);
+  ul.appendChild(newPuppyLI);
+});
